eab14dc9dc9d724f00327882aa6baffc6a907d56
YanariYoukai <krucina.j@seznam.cz>
2022-02-28 14:03:45 +0100

    a1

---
+++ src/GreetMethod.java
@@ -0,0 +1,11 @@
+public class GreetMethod {
+    private String name;
+
+    public GreetMethod() {
+        this.name = "Yanari Youkai";
+    }
+
+    public void greet() {
+        System.out.printf("Hello, my name is %s", name);
+    }
+}
