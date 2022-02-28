ea3652a709531d7d5eb97a72783da72d2a202e82
stanislav <stanno00@gmail.com>
2022-02-28 15:35:11 +0100

    d5 2

--- src/Future.java
+++ src/Future.java
@@ -2,7 +2,7 @@ public class Future {
 
     public void future() {
         String sign = "vahy";
-        String nieco = "zde";
+        String nieco = "daco";
         System.out.println(nieco);
 
         System.out.println(sign);
